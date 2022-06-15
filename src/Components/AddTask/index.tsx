8b7d723a70c01c91a1c24d9a04456c1275d58f96
Reza Bozorgi <rezab@qpage.one>
2022-06-15 18:33:26 +0430

    feat: add addtask ui

--- src/Components/AddTask/index.tsx
+++ src/Components/AddTask/index.tsx
@@ -1,7 +1,107 @@
 import React from 'react';
+import {
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from '@mui/material';
+import { green, red, yellow } from '@mui/material/colors';
+import theme from '../../Theme/default';
 
 const AddTask = () => {
-  return <div>index</div>;
+  const [selectedPriority, setSelectedPriority] = React.useState('Low');
+
+  const handlePriorityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedPriority(event.target.value);
+  };
+
+  const priorityProps = (item: string) => ({
+    checked: selectedPriority === item,
+    onChange: handlePriorityChange,
+    value: item,
+    name: `priority-radio-${item}`,
+    inputProps: { 'aria-label': item },
+    fontSize: 128,
+  });
+
+  return (
+    <>
+      <TextField name='task-tile' placeholder='Task Title' fullWidth />
+      <TextField
+        name='task-description'
+        placeholder='Task Description'
+        multiline
+        fullWidth
+        minRows={4}
+      />
+      <TextField
+        name='task-extra'
+        placeholder='Gifts and KPI for this task ;)'
+        fullWidth
+      />
+
+      <RadioGroup
+        row
+        name='row-radio-buttons-group'
+        sx={{ justifyContent: 'space-between', minWidth: '100vw' }}
+      >
+        <FormControlLabel
+          value='Low'
+          control={
+            <Radio
+              {...priorityProps('Low')}
+              sx={{
+                color: theme.palette.secondary.main,
+                '& .MuiSvgIcon-root': {
+                  fontSize: 60,
+                },
+                '&.Mui-checked': {
+                  color: red[900],
+                },
+              }}
+            />
+          }
+          label='Low'
+        />
+        <FormControlLabel
+          value='Medium'
+          control={
+            <Radio
+              {...priorityProps('Meidum')}
+              sx={{
+                color: theme.palette.secondary.main,
+                '& .MuiSvgIcon-root': {
+                  fontSize: 60,
+                },
+                '&.Mui-checked': {
+                  color: yellow[500],
+                },
+              }}
+            />
+          }
+          label='Medium'
+        />
+        <FormControlLabel
+          value='High'
+          control={
+            <Radio
+              {...priorityProps('High')}
+              sx={{
+                color: theme.palette.secondary.main,
+                '& .MuiSvgIcon-root': {
+                  fontSize: 60,
+                },
+                '&.Mui-checked': {
+                  color: green[500],
+                },
+              }}
+            />
+          }
+          label='High'
+        />
+      </RadioGroup>
+    </>
+  );
 };
 
 export default AddTask;
--- src/Containers/Modal/index.tsx
+++ src/Containers/Modal/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, useTheme } from '@mui/material';
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,12 +8,24 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  const theme = useTheme();
   return (
-    <>
+   <>
+   <Box></Box>
+    <Box
+      sx={{
+        borderRadius: '24px 10px',
+        backgroundColor: theme.palette.secondary.main,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems:'center'
+      }}
+      >
       {React.Children.map(children, (child) => {
         return React.cloneElement(child as React.ReactElement<any>);
       })}
-    </>
+    </Box>
+      </>
   );
 };
 
