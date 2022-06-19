524b1357abfa5bcf027b1a09111716c3b71afe16
Reza Bozorgi <rezab@qpage.one>
2022-06-19 23:24:32 +0430

    feat: add error boundary to catch all error and error text while an error throw

---
+++ src/ErrorBoundary/index.tsx
@@ -0,0 +1,39 @@
+import React, { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  // eslint-disable-next-line react/require-default-props
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  // eslint-disable-next-line react/state-in-constructor
+  public state: State = {
+    hasError: false,
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    // eslint-disable-next-line react/destructuring-assignment
+    if (this.state.hasError) {
+      return <h1>Sorry.. there was an error</h1>;
+    }
+
+    // eslint-disable-next-line react/destructuring-assignment
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
